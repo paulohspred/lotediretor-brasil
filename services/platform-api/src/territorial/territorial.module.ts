6fda2032046bcff4a7e29bfd63e04804f814a056
paulohspred <100045106+paulohspred@users.noreply.github.com>
2026-08-24 08:24:58 -0300

    fix(runtime): import auth provider into territorial module

--- services/platform-api/src/territorial/territorial.module.ts
+++ services/platform-api/src/territorial/territorial.module.ts
@@ -1,7 +1,13 @@
 import {Module} from '@nestjs/common';
+import {CoreModule} from '../core.module';
 import {TerritorialController} from './territorial.controller';
 import {TerritorialService} from './territorial.service';
 import {AnalysisV20Service} from './analysis-v20.service';
 
-@Module({controllers:[TerritorialController],providers:[TerritorialService,AnalysisV20Service],exports:[TerritorialService,AnalysisV20Service]})
+@Module({
+  imports:[CoreModule],
+  controllers:[TerritorialController],
+  providers:[TerritorialService,AnalysisV20Service],
+  exports:[TerritorialService,AnalysisV20Service],
+})
 export class TerritorialModule{}

19b8ef21e4f16a2b1f854253412807039c89d7bc
paulohspred <100045106+paulohspred@users.noreply.github.com>
2026-08-26 09:22:33 -0300

    feat(privacy): wire LGPD module into platform API

--- services/platform-api/src/app.module.ts
+++ services/platform-api/src/app.module.ts
@@ -11,6 +11,7 @@ import {RuralModule} from './rural/rural.module';
 import {CondoModule} from './condo/condo.module';
 import {SolarModule} from './solar/solar.module';
 import {MunicipalityModule} from './municipality/municipality.module';
+import {PrivacyModule} from './privacy/privacy.module';
 
-@Module({imports:[CoreModule,TerritorialModule,Property360Module,ReportModule,RuralModule,CondoModule,SolarModule,MunicipalityModule,DomainsModule,MetricsModule,ContractsModule,JobEventsModule]})
+@Module({imports:[CoreModule,TerritorialModule,Property360Module,ReportModule,RuralModule,CondoModule,SolarModule,MunicipalityModule,PrivacyModule,DomainsModule,MetricsModule,ContractsModule,JobEventsModule]})
 export class AppModule{}

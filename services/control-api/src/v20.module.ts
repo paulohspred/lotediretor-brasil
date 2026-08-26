2c9d896ae0340cf12fcc4fd9e21a582fb9099858
paulohspred <100045106+paulohspred@users.noreply.github.com>
2026-08-26 11:51:25 -0300

    feat(billing): wire paid-invoice entitlement bridge

--- services/control-api/src/v20.module.ts
+++ services/control-api/src/v20.module.ts
@@ -1,6 +1,7 @@
 import {Module} from '@nestjs/common';
 import {AppModule} from './app.module';
 import {ControlV20AdminController,ControlV20InternalController,ControlV20PublicController} from './v20-admin.controller';
+import {BillingEntitlementController} from './billing-entitlement.controller';
 
-@Module({imports:[AppModule],controllers:[ControlV20AdminController,ControlV20InternalController,ControlV20PublicController]})
+@Module({imports:[AppModule],controllers:[ControlV20AdminController,ControlV20InternalController,ControlV20PublicController,BillingEntitlementController]})
 export class ControlV20Module{}
